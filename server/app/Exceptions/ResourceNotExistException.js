48831fe0522f4a3ec08055f58811ddce048186da
rvranjes <robertvranjes24@gmail.com>
2021-05-25 22:54:01 +0200

    destroy with exceptions

---
+++ server/app/Exceptions/ResourceNotExistException.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const { LogicalException } = require('@adonisjs/generic-exceptions')
+
+class ResourceNotExistException extends LogicalException {
+  /**
+   * Handle this exception by itself
+   */
+  
+   handle (error, { response }) {
+    return response.status(403).json({
+      error: 'the resource did not exist',
+    });
+  }
+}
+
+module.exports = ResourceNotExistException
---
+++ server/app/Services/AuthorizationService.js
@@ -0,0 +1,15 @@
+const InvalidAccessException = use('App/Exceptions/InvalidAccessException');
+
+class AuthorizationService {
+    verifyPermission(resource, user) {
+        if (!resource) {
+            throw new ResourceNotExistException();
+        }
+        
+        if (resource.user_id !== user.id) {
+            throw new InvalidAccessException();
+        }
+    }
+}
+
+module.exports = new AuthorizationService();
