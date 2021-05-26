5e59e53f0a98867541246e62373a3165bed96faa
rvranjes <robertvranjes24@gmail.com>
2021-05-27 01:03:36 +0200

    initial vue setup

--- client/src/main.js
+++ client/src/main.js
@@ -4,13 +4,12 @@ import Vuetify from 'vuetify';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/index';
 
 Vue.config.productionTip = false;
 
 Vue.use(Vuetify);
 sync(store, router);
-
 
 new Vue({
   router,
